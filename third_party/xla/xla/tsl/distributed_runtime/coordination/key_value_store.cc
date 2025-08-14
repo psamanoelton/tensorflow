anisuli787/Anisul

Add atomic increment functionality to `KeyValueStore`.
Implement `KeyValueStore::IncrementBy` to atomically increment a key's value, which is stored as a big-endian `int64_t`.

PiperOrigin-RevId: 794845644

third_party/xla/xla/tsl/distributed_runtime/coordination/key_value_store.cc
@@ -15,11 +15,13 @@ limitations under the License.
 
 #include "xla/tsl/distributed_runtime/coordination/key_value_store.h"
 
+#include <cstdint>
 #include <optional>
 #include <string>
 #include <utility>
 #include <vector>
 
+#include "absl/base/internal/endian.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/match.h"
@@ -65,6 +67,27 @@ std::optional<std::string> KeyValueStore::Get(absl::string_view key) {
   if (it == data_.end()) {
     return std::nullopt;
   }
+  return it->second;
+}
+
+absl::StatusOr<std::string> KeyValueStore::IncrementBy(absl::string_view key,
+                                                       int64_t increment) {
+  absl::MutexLock l(mu_);
+
+  auto [it, inserted] = data_.try_emplace(key, "");
+  if (inserted) {
+    std::string new_value(sizeof(int64_t), '\0');
+    it->second = std::move(new_value);
+  } else if (it->second.size() != sizeof(int64_t)) {
+    return absl::InternalError(absl::StrCat(
+        "key ", key, " has an unexpected value size: ", it->second.size(),
+        " bytes, expected ", sizeof(int64_t), " bytes."));
+  }
+  int64_t new_value =
+      static_cast<int64_t>(absl::big_endian::Load64(it->second.data())) +
+      increment;
+
+  absl::big_endian::Store64(it->second.data(), new_value);
   return it->second;
 }
 

third_party/xla/xla/tsl/distributed_runtime/coordination/key_value_store_test.cc
@@ -202,5 +202,39 @@ TEST(KeyValueStore, CallbacksCalledOnDestruction) {
   EXPECT_TRUE(callback_called);
 }
 
+TEST(KeyValueStore, IncrementByPositiveValueSucceeds) {
+  KeyValueStore store;
+  ASSERT_OK(store.Put("foo",
+                      absl::string_view("\x00\x00\x00\x00\x00\x00\x00\x03", 8),
+                      /*allow_overwrite=*/true));
+  EXPECT_THAT(store.IncrementBy("foo", 2),
+              IsOkAndHolds(std::string("\x00\x00\x00\x00\x00\x00\x00\x05", 8)));
+}
+
+TEST(KeyValueStore, IncrementByNegativeValueSucceeds) {
+  KeyValueStore store;
+  ASSERT_OK(store.Put("foo",
+                      absl::string_view("\x00\x00\x00\x00\x00\x00\x00\x02", 8),
+                      /*allow_overwrite=*/true));
+  // Solution will be the two's complement of the positive value.
+  EXPECT_THAT(store.IncrementBy("foo", -5),
+              IsOkAndHolds(std::string("\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFD", 8)));
+}
+
+TEST(KeyValueStore, IncrementByWithoutExistingValueSucceeds) {
+  // Solution will be the increment value. This behavior is consistent with
+  // other KV store implementations.
+  KeyValueStore store;
+  EXPECT_THAT(store.IncrementBy("foo", 2),
+              IsOkAndHolds(std::string("\x00\x00\x00\x00\x00\x00\x00\x02", 8)));
+}
+
+TEST(KeyValueStore, IncrementByWithInvalidValueFails) {
+  KeyValueStore store;
+  ASSERT_OK(store.Put("foo", "invalid", /*allow_overwrite=*/true));
+  EXPECT_THAT(store.IncrementBy("foo", 2),
+              StatusIs(absl::StatusCode::kInternal));
+}
+
 }  // namespace
 }  // namespace tsl

psamanoelton/tensorflow

third_party/xla/xla/tsl/distributed_runtime/coordination/key_value_store.cc
@@ -15,11 +15,13 @@ limitations under the License.
 
 #include "xla/tsl/distributed_runtime/coordination/key_value_store.h"
 
+#include <cstdint>
 #include <optional>
 #include <string>
 #include <utility>
 #include <vector>
 
+#include "absl/base/internal/endian.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/match.h"
@@ -65,6 +67,28 @@ std::optional<std::string> KeyValueStore::Get(absl::string_view key) {
   if (it == data_.end()) {
     return std::nullopt;
   }
+  return it->second;
+}
+
+absl::StatusOr<std::string> KeyValueStore::IncrementBy(absl::string_view key,
+                                                       int64_t increment) {
+  absl::MutexLock l(mu_);
+
+  auto [it, inserted] = data_.try_emplace(key, "");
+  if (inserted) {
+    std::string new_value(sizeof(int64_t), '\0');
+    it->second = std::move(new_value);
+  } else if (it->second.size() != sizeof(int64_t)) {
+    return absl::FailedPreconditionError(absl::StrCat(
+        "key ", key, " has an unexpected value size: ", it->second.size(),
+        " bytes, expected ", sizeof(int64_t), " bytes."));
+  }
+  int64_t new_value =
+      static_cast<int64_t>(absl::big_endian::Load64(it->second.data())) +
+      increment;
+
+  absl::big_endian::Store64(it->second.data(), new_value);
+  NotifyCallbacksForKey(key, it->second);
   return it->second;
 }
 

third_party/xla/xla/tsl/distributed_runtime/coordination/key_value_store_test.cc
@@ -202,5 +202,39 @@ TEST(KeyValueStore, CallbacksCalledOnDestruction) {
   EXPECT_TRUE(callback_called);
 }
 
+TEST(KeyValueStore, IncrementByPositiveValueSucceeds) {
+  KeyValueStore store;
+  ASSERT_OK(store.Put("foo",
+                      absl::string_view("\x00\x00\x00\x00\x00\x00\x00\x03", 8),
+                      /*allow_overwrite=*/true));
+  EXPECT_THAT(store.IncrementBy("foo", 2),
+              IsOkAndHolds(std::string("\x00\x00\x00\x00\x00\x00\x00\x05", 8)));
+}
+
+TEST(KeyValueStore, IncrementByNegativeValueSucceeds) {
+  KeyValueStore store;
+  ASSERT_OK(store.Put("foo",
+                      absl::string_view("\x00\x00\x00\x00\x00\x00\x00\x02", 8),
+                      /*allow_overwrite=*/true));
+  // Result will be the two's complement of the positive value.
+  EXPECT_THAT(store.IncrementBy("foo", -5),
+              IsOkAndHolds(std::string("\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFD", 8)));
+}
+
+TEST(KeyValueStore, IncrementByWithoutExistingValueSucceeds) {
+  // Result will be the increment value. This behavior is consistent with
+  // other KV store implementations.
+  KeyValueStore store;
+  EXPECT_THAT(store.IncrementBy("foo", 2),
+              IsOkAndHolds(std::string("\x00\x00\x00\x00\x00\x00\x00\x02", 8)));
+}
+
+TEST(KeyValueStore, IncrementByWithInvalidValueFails) {
+  KeyValueStore store;
+  ASSERT_OK(store.Put("foo", "invalid", /*allow_overwrite=*/true));
+  EXPECT_THAT(store.IncrementBy("foo", 2),
+              StatusIs(absl::StatusCode::kFailedPrecondition));
+}
+
 }  // namespace
 }  // namespace tsl